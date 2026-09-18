bbb4212b180db2f3a72e94578df1bf3215bca626
Kagerou_draw <275000717+briansedano08-creator@users.noreply.github.com>
2026-09-18 02:42:33 -0500

    Refactor suit rendering to use Rider data
    Updated SuitRenderLayer to use RiderDefinition and RiderRegistry for suit rendering.

--- src/main/java/com/neroferno/krm_revo/client/renderer/layer/SuitRenderLayer.java
+++ src/main/java/com/neroferno/krm_revo/client/renderer/layer/SuitRenderLayer.java
@@ -2,8 +2,9 @@ package com.neroferno.krm_revo.client.renderer.layer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.neroferno.krm_revo.client.renderer.SuitVisualRenderer;
-import com.neroferno.krm_revo.item.ModItems;
 import com.neroferno.krm_revo.network.TransformationHelper;
+import com.neroferno.krm_revo.rider.RiderDefinition;
+import com.neroferno.krm_revo.rider.RiderRegistry;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -21,6 +22,7 @@ public class SuitRenderLayer extends RenderLayer<AbstractClientPlayer, PlayerMod
     
     private final SuitVisualRenderer renderer;
     private ItemStack dummySuitStack;
+    private String cachedRiderId;
 
     public SuitRenderLayer(RenderLayerParent<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> renderLayerParent) {
         super(renderLayerParent);
@@ -31,15 +33,21 @@ public class SuitRenderLayer extends RenderLayer<AbstractClientPlayer, PlayerMod
     public void render(PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, AbstractClientPlayer player, float limbSwing, float limbSwingAmount, float partialTick, float ageInTicks, float netHeadYaw, float headPitch) {
         if (!TransformationHelper.isTransformed(player)) return;
 
-        if (this.dummySuitStack == null) {
-            this.dummySuitStack = new ItemStack(ModItems.KUUGA_SUIT_VISUAL.get());
+        String activeRiderId = TransformationHelper.getActiveRiderId(player);
+        RiderDefinition rider = RiderRegistry.get(activeRiderId);
+        if (rider == null) return;
+
+        // Re-create the dummy stack only when the active Rider actually changed
+        if (this.dummySuitStack == null || !activeRiderId.equals(this.cachedRiderId)) {
+            this.dummySuitStack = new ItemStack(rider.getSuit());
+            this.cachedRiderId = activeRiderId;
         }
 
         // Prepare the GeckoLib renderer to sync rotations with the vanilla player model skeleton
         this.renderer.prepForRender(player, dummySuitStack, EquipmentSlot.CHEST, this.getParentModel());
-        
-        RenderType renderType = this.renderer.getRenderType((SuitVisualItem) dummySuitStack.getItem(), 
-                                                            ResourceLocation.fromNamespaceAndPath("krm_revo", "textures/armor/kuuga_suit.png"), 
+
+        RenderType renderType = this.renderer.getRenderType((SuitVisualItem) dummySuitStack.getItem(),
+                                                            rider.getSuitTexture(),
                                                             bufferSource, partialTick);
         VertexConsumer vertexConsumer = bufferSource.getBuffer(renderType);
 
